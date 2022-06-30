ec9f5bcb0a91b2d94f47a2ef533d78d17523803b
Le Duc Anh <leducanh.hna@gmail.com>
2022-06-30 21:01:55 +0700

    first commit

---
+++ asset/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(".icon-bar").click(function () {
+        $(".menu").toggleClass("menu1");
+    });
+});
+document.addEventListener("contextmenu", event => event.preventDefault()
+); //// chặn dùng chuột phải
+document.addEventListener("keydown", function (event){ ////// chặn f12
+if (event.ctrlKey){
+   event.preventDefault();
+}
+if(event.keyCode == 123){
+   event.preventDefault();
+}
+})
