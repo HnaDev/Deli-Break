3d29033adf8012cf780187a16e37d74b516df02e
Le Duc Anh <99636699+hnaDeveloper@users.noreply.github.com>
2022-07-01 15:31:41 +0700

    Update app.js

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
         $(".menu").toggleClass("menu1");
     });
 });
-document.addEventListener("contextmenu", event => event.preventDefault()
-); //// chặn dùng chuột phải
-document.addEventListener("keydown", function (event){ ////// chặn f12
-if (event.ctrlKey){
-   event.preventDefault();
-}
-if(event.keyCode == 123){
-   event.preventDefault();
-}
-})
+// document.addEventListener("contextmenu", event => event.preventDefault()
+// ); //// chặn dùng chuột phải
+// document.addEventListener("keydown", function (event){ ////// chặn f12
+// if (event.ctrlKey){
+//    event.preventDefault();
+// }
+// if(event.keyCode == 123){
+//    event.preventDefault();
+// }
+// })
