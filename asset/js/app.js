5d3733f401241b25f2cd89d708fc4e14f68c1339
Le Duc Anh <99636699+hnaDeveloper@users.noreply.github.com>
2022-07-01 15:33:38 +0700

    Update app.js

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,13 +3,4 @@ $(document).ready(function () {
         $(".menu").toggleClass("menu1");
     });
 });
-// document.addEventListener("contextmenu", event => event.preventDefault()
-// ); //// chặn dùng chuột phải
-// document.addEventListener("keydown", function (event){ ////// chặn f12
-// if (event.ctrlKey){
-//    event.preventDefault();
-// }
-// if(event.keyCode == 123){
-//    event.preventDefault();
-// }
-// })
+
